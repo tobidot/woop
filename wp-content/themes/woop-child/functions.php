4cdbbe77d1a1ceb2b51be2c96725263b3a3a0f4a
tobidot <Object.name@live.de>
2020-07-19 10:37:25 +0200

    fix the functions php import for the child theme

--- wp-content/themes/woop-child/functions.php
+++ wp-content/themes/woop-child/functions.php
@@ -1,9 +1,9 @@
 <?php
 
-add_action('after_theme_setup', function () {
-    woop_helper\require_once_directory('inc');
+add_action('after_setup_theme', function () {
+    woop_helper\require_once_directory(__DIR__ . '/inc');
 
-    woop_helper\require_once_directory('templates');
+    woop_helper\require_once_directory(__DIR__ . '/templates/partials');
 
-    woop_helper\require_once_directory('templates/partials');
+    woop_helper\require_once_directory(__DIR__ . '/templates');
 });
